7a2f43908985590ab8b904004cd4c41e341216be
Christopher Wellons <wellons@nullprogram.com>
2016-05-25 21:21:06 -0400

    Bump to 1.1.0.

--- x86-lookup.el
+++ x86-lookup.el
@@ -4,7 +4,7 @@
 
 ;; Author: Christopher Wellons <wellons@nullprogram.com>
 ;; URL: https://github.com/skeeto/x86-lookup
-;; Version: 1.0.0
+;; Version: 1.1.0
 ;; Package-Requires: ((emacs "24.3") (cl-lib "0.3"))
 
 ;;; Commentary:
