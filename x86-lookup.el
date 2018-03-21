3fa5c28ad57532851a29d92be328d3d39568fb17
Christopher Wellons <wellons@nullprogram.com>
2018-03-21 17:30:27 -0400

    Split overly-long regular expression

--- x86-lookup.el
+++ x86-lookup.el
@@ -139,7 +139,8 @@ This function accepts two arguments: filename and page number."
   "Create an index alist from PDF mapping mnemonics to page numbers.
 This function requires the pdftotext command line program."
   (let ((mnemonic (concat "\\(?:.*\n\n?\\)?"
-                          "\\([[:alnum:]/[:blank:]]+\\)[[:blank:]]*\\(?:--\\|—\\)\\(?:.*\n\n?\\)\\{1,3\\}"
+                          "\\([[:alnum:]/[:blank:]]+\\)[[:blank:]]*"
+                          "\\(?:--\\|—\\)\\(?:.*\n\n?\\)\\{1,3\\}"
                           "[[:blank:]]*Opcode"))
         (coding-system-for-read 'utf-8)
         (coding-system-for-write 'utf-8)
